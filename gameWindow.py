1a46993e48429ec58367b98b9dfb07e3040b0fa7
Logan Finley <finley4.logan@gmail.com>
2022-12-06 17:18:10 -0800

    Changed the point at which the paddles are displayed

--- gameWindow.py
+++ gameWindow.py
@@ -10,8 +10,10 @@ class GameWindow:
         self.__window = turtle.Screen()
         self.__window.setup(self.__width, self.__height)
 
-        self.__player1 = Paddle(1, -self.__width/2)
-        self.__player2 = Paddle(2, self.__width/2)
+        self.__player1 = Paddle(-self.__width/3)
+        self.__player2 = Paddle(self.__width/3)
+        self.__player1.show()
+        self.__player2.show()
         #self.__player2.setX(self.__width - self.__width/100)
 
         self.__window.onkey(self.__player1.moveUp, 'w')
@@ -26,18 +28,6 @@ class GameWindow:
 
     def getHeight():
         return self.__height
-
-    def p1Up(self):
-        self.__player1.moveUp()
-
-    def p1Down(self):
-        self.__player1.moveDown()
-
-    def p2Up(self):
-        self.__player2.moveUp()
-
-    def p2Down(self):
-        self.__player2.moveDown()
 
     def mainloop(self):
         self.__window.mainloop()
--- paddle.py
+++ paddle.py
@@ -4,36 +4,23 @@ class Paddle:
     #self.__player1Pos = (-10, 0)
     #self.__player2Pos = (10, 0)
 
-    def __init__(self, player, xPos):
-        '''
-        if player == 1:
-            self.__xPos, self.__yPos = self.__player1Pos
-        else:
-            self.__xPos, self.__yPos = self.__player2Pos
-        '''
+    def __init__(self, xPos):
+        self.__distance = 20
+        self.__paddle = turtle.Turtle(visible = False)
 
-        self.__paddle = turtle.Turtle()
-
-        self.__paddle.hideturtle()
         self.__paddle.penup()
         self.__paddle.setx(xPos)
-        self.__paddle.pendown()
         self.__paddle.shape("square")
         self.__paddle.setheading(90)
         self.__paddle.shapesize(1, 6)
         self.__paddle.color("grey")
         self.__paddle.speed(0)
-        self.__paddle.penup()
-        self.__paddle.showturtle()
 
-        self.__distance = 20
+    def show(self):
+        self.__paddle.showturtle()
 
     def moveUp(self):
         self.__paddle.forward(self.__distance)
 
     def moveDown(self):
         self.__paddle.backward(self.__distance)
-
-    def setX(self, xPos):
-        self.__paddle.hideturtle()
-        #self.__paddle.setx(xPos)
