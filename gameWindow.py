090b4025053f656c26a6150153e136545ec43cd7
Logan Finley <finley4.logan@gmail.com>
2022-12-07 18:22:12 -0800

    Drew a simple court and figured out some basic collision checking between paddles and top/bottom borders

--- gameWindow.py
+++ gameWindow.py
@@ -14,30 +14,48 @@ class GameWindow:
         self.__window = turtle.Screen()
         self.__window.setup(self.__width, self.__height)
 
-        self.__player1 = Paddle(-self.__width/3)
-        self.__player2 = Paddle(self.__width/3)
-        self.__player1.show()
-        self.__player2.show()
+        self.__leftBorder = (-self.__width / 2) + 10
+        self.__rightBorder = (self.__width / 2) - 20
+        self.__topBorder = (self.__height / 2) - 10
+        self.__bottomBorder = (-self.__height / 2) + 20
+        self.drawCourt()
 
         self.__wKey = KeyEvent('w')
         self.__sKey = KeyEvent('s')
         self.__upKey = KeyEvent('Up')
         self.__downKey = KeyEvent('Down')
 
+        self.__player1 = Paddle(-self.__width/2.5)
+        self.__player2 = Paddle(self.__width/2.5)
+        self.__player1.show()
+        self.__player2.show()
+
         self.__square = Square()
+
+    def drawCourt(self):
+        self.__court = turtle.Turtle(visible=False)
+        self.__court.speed(0)
+        self.__court.pensize(5)
+        self.__court.penup()
+        self.__court.goto(self.__leftBorder, self.__topBorder)
+        self.__court.pendown()
+        self.__court.goto(self.__leftBorder, self.__bottomBorder)
+        self.__court.goto(self.__rightBorder, self.__bottomBorder)
+        self.__court.goto(self.__rightBorder, self.__topBorder)
+        self.__court.goto(self.__leftBorder, self.__topBorder)
 
     def move(self):
         if self.__wKey.down and not self.__sKey.down:
-            self.__player1.moveUp()
+            self.__player1.moveUp(self.__topBorder)
 
         if self.__sKey.down and not self.__wKey.down:
-            self.__player1.moveDown()
+            self.__player1.moveDown(self.__bottomBorder)
 
         if self.__upKey.down and not self.__downKey.down:
-            self.__player2.moveUp()
+            self.__player2.moveUp(self.__topBorder)
 
         if self.__downKey.down and not self.__upKey.down:
-            self.__player2.moveDown()
+            self.__player2.moveDown(self.__bottomBorder)
 
         self.__window.ontimer(self.move, 10)
 
--- paddle.py
+++ paddle.py
@@ -12,12 +12,25 @@ class Paddle:
         self.__paddle.setheading(90)
         self.__paddle.shapesize(1, 6)
         self.__paddle.color("grey")
+        self.__bottom = 63 # need to find a generic way to determine these values
+        self.__top = 63
 
     def show(self):
         self.__paddle.showturtle()
 
-    def moveUp(self):
-        self.__paddle.forward(self.__distance)
+    def moveUp(self, topBorder):
+        if self.__paddle.ycor() + self.__top == topBorder:
+            pass
+        elif self.__paddle.ycor() + self.__top + self.__distance >= topBorder:
+            self.__paddle.forward(topBorder - self.__paddle.ycor() - self.__top)
+        elif self.__paddle.ycor() + self.__top <= topBorder:
+            self.__paddle.forward(self.__distance)
 
-    def moveDown(self):
-        self.__paddle.backward(self.__distance)
+
+    def moveDown(self, bottomBorder):
+        if self.__paddle.ycor() - self.__bottom == bottomBorder:
+            pass
+        elif self.__paddle.ycor() - self.__bottom - self.__distance <= bottomBorder:
+            self.__paddle.backward(self.__paddle.ycor() - self.__bottom - bottomBorder)
+        elif self.__paddle.ycor() - self.__bottom  >= bottomBorder:
+            self.__paddle.backward(self.__distance)
